a4ff6a7033400570384094f7a03682070722de06
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-09-15 21:28:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// TASK_1
+
+Console.WriteLine("TASK 1");
+object[] collection = { 1, "hello", 3.14, DateTime.Now };
+Thread thread = new Thread(() =>
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.WriteLine(collection[i].GetType()+"\t\t"+collection[i].ToString());
+    }
+});
+thread.Start();
+thread.Join();
+
+// TASK_2
+
